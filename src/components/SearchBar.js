36f42623d1b097b393fd178a4e24159c0368af7d
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-02-09 16:42:17 +0100

    Finish search suggestions functionality.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,40 +1,42 @@
 /* eslint-disable react/prop-types */
-import React from "react";
-// import DropDown from "./DropDown";
+import React, { useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 
 const SearchBar = ({ videoIds }) => {
+  const [filteredList, setFilteredList] = useState([]);
 
-  const options = videoIds.map((video) => {
-    const container = {};
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    const newFilter = videoIds.filter((video) => {
+      return video.id.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
 
-    container.name = video.id.name;
-    container.id = video.id.videoId;
-
-    return container;
-  });
+    searchWord === "" ? setFilteredList([]) : setFilteredList(newFilter);
+  };
 
   return (
     <div className="search-bar">
       <div className="search-bar__input">
         <input
-          type = "search"
-          name = "q"
+          type="type"
           className="search-bar__input-field"
           placeholder="Search a video"
           aria-label="Search a video"
+          onChange={handleFilter}
         />
         <button className="search-button" type="button">
           <BiSearchAlt />
         </button>
-      </div>      
-      <div className="drop-down__list">
-      {options.map((option) => 
-        <button key={option.id} className="drop-down__btn-video">
-          {option.name}
-        </button>
+      </div>
+      {filteredList.length !== 0 && (
+        <div className="drop-down__list">
+          {filteredList.slice(0, 5).map((video) => (
+            <button key={video.id.videoId} className="drop-down__btn-video">
+              {video.id.name}
+            </button>
+          ))}
+        </div>
       )}
-    </div>
     </div>
   );
 };
