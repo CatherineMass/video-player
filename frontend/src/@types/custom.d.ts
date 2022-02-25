b37fcbec41ed62ef91acbc096f7a5d9163195963
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-02-25 13:15:16 +0100

    Fix error .svg in MainContainer. No errors left.

--- frontend/src/@types/custom.d.ts
+++ frontend/src/@types/custom.d.ts
@@ -1,11 +1,3 @@
-import React, { AriaAttributes, DOMAttributes } from 'react';
-
-declare module 'react' {
-  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    alt?: string;
-  }
-}
-
 declare module '*.svg' {
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
   const src: string;
