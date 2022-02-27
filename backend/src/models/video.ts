7d7bf6393bac8380743cb0d1f7af55845698eb38
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-02-27 16:15:29 +0100

    Add objection model for video.

---
+++ backend/src/models/video.ts
@@ -0,0 +1,27 @@
+import { Model } from 'objection';
+
+class Video extends Model {
+	static get tableName() {
+		return 'videos';
+	}
+
+	static get jsonSchema() {
+		return {
+			type: 'object',
+			properties: {
+				kind: { type: 'string' },
+				etag: { type: 'string' },
+				id: {
+					type: 'object',
+					properties: {
+						kind: { type: 'string' },
+						videoId: { type: 'string' },
+						name: { type: 'string' },
+					},
+				},
+			},
+		};
+	}
+}
+
+export default Video;
