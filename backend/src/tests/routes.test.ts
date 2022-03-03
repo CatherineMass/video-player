e1e45aad7d92d1529e50b7feb2aabf25702f7270
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-03-03 12:59:39 +0100

    Add afterAll to disconnect from db but doesn't work.

--- backend/src/tests/routes.test.ts
+++ backend/src/tests/routes.test.ts
@@ -1,8 +1,12 @@
 import express, { Application } from 'express';
+// import knex from 'knex';
+import {raw} from 'objection';
 import request from 'supertest';
+// import Video from '../models/video';
 import router from '../routes/routes';
 
 const app: Application = express();
+// const knex = Video.knex();
 
 describe('GET /', () => { 
 	describe('get all the videos from the database', () => { 
@@ -10,5 +14,11 @@ describe('GET /', () => {
 			const response = await request(app.use(router)).get('/');
 			expect(response.statusCode).toBe(200);
 		});
+		afterAll(async () => {
+			await raw('\\c postgres');
+		});
+
 	});
 });
+
+// ==> need a global setup (that creates a test db, connection, migration, seed) & global teardown (drops the test db)
