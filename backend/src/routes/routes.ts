c84a8c3a9aaed212de06256aa82f2a8c6c921f8c
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-03-10 16:25:26 +0100

    Fix search route to use only one loop.

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -16,14 +16,6 @@ interface Item {
     }
 }
 
-interface VideoItem {
-    etag: string;
-    id: {
-        videoId: string;
-        name: string;
-    }
-}
-
 // // Initialize knex
 const knex = Knex(config.development);
 
@@ -63,71 +55,28 @@ router.route('/search').get(async (req: Request, res: Response) => {
 
     // API connection to Youtube
     // Limit of 2 videos is just for now. Will increase it once the feature is working.
-    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=portugal&relevanceLanguage=en&type=video&videoEmbeddable=true&key=${process.env.API_KEY}`);
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=colombia&relevanceLanguage=en&type=video&videoEmbeddable=true&key=${process.env.API_KEY}`);
     const data = await response.json();
     const items: Item[] = data.items;
-    
-    const results = async (items: Item[]) => {
-        const searchResult: VideoItem[] = [];
-        for (let i = 0; i < items.length; i++) {
-            const toInsert = {
-                etag: items[i].etag, 
-                videoId: items[i].id.videoId, 
-                name: items[i].snippet.title
-            };
-            await Video.query().insert(toInsert);
-            searchResult.push({
-                etag: items[i].etag,
-                id: {
-                    videoId: items[i].id.videoId,
-                    name: items[i].snippet.title,
-                }
-            });
-        }
-        return searchResult;
-    };
+        
+    const promises = items.map(async item => {
+        const toInsert = await {
+            etag: item.etag, 
+            videoId: item.id.videoId, 
+            name: item.snippet.title
+        };
+        await Video.query().insert(toInsert);
+        return {
+            etag: item.etag,
+            id: {
+                videoId: item.id.videoId,
+                name: item.snippet.title,
+            },
+        };
+    });
+    const toSend = await Promise.all(promises);   
 
-    const toSend = results(items);
-    return res.send(toSend);
-
-    // const resul = items.map(async item => {
-    //     const toInsert = {
-    //         etag: item.etag, 
-    //         videoId: item.id.videoId, 
-    //         name: item.snippet.title
-    //     };
-    //     await Video.query().insert(toInsert);
-    //     return {
-    //         etag: item.etag,
-    //         id: {
-    //             videoId: item.id.videoId,
-    //             name: item.snippet.title,
-    //         }
-    //     };
-    // });
-
-
-    // save to database
-    // const result = items.map(item => {
-    //     Video.query().insertAndFetch({
-    //         etag: item.etag, 
-    //         videoId: item.id.videoId, 
-    //         name: item.snippet.title
-    //     });
-    // });
-    // return res.send(result);
-
-    // await Video.query().insert(videosInsert);
-
-    // send result    
-    // const searchResult = items.map(item => ({
-    //     etag: item.etag,
-    //     id: {
-    //         videoId: item.id.videoId,
-    //         name: item.snippet.title,
-    //     }
-    // }));
-    // return res.send(searchResult);
+    return res.status(201).json({toSend});
 });
 
 export default router;
