76d5cb9d4851432f87786c9eb2a6e7884736f5a8
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-03-13 17:02:46 +0100

    Add some styling for login and signup.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <form>
-      <input title='username' placeholder='Username' type='text' />
-      <input title='password' placeholder='Password' type='text' />
+    <form className='form login'>
+      <label className='form-label'>Username</label>
+      <input title='username' placeholder='Username' type='text' className='form-text-input' />
+      <label className='form-label'>Password</label>
+      <input title='password' placeholder='Password' type='text' className='form-text-input' />
       <button>Submit</button>
     </form>
   );
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 const Signup = () => {
   return (
     <div>
-      <form>
-        <input title='username' placeholder='Username' type='text' />
-        <input title='email' placeholder='Email' type='text' />
-        <input title='password' placeholder='Password' type='text' />
+      <form className='form signup'>
+        <label className='form-label'>Username</label>
+        <input title='username' placeholder='Username' type='text' className='form-text-input' />
+        <label className='form-label'>Email</label>
+        <input title='email' placeholder='Email' type='text' className='form-text-input' />
+        <label className='form-label'>Password</label>
+        <input title='password' placeholder='Password' type='text' className='form-text-input' />
         <button>Submit</button>
       </form>
     </div>
