28bc8b65663991376f818bda815397cf2d539a08
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-03-13 20:35:03 +0100

    Finish form style for login and signup.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 const Login = () => {
   return (
     <form className='form login'>
+      <h1 className='form-title'>Login</h1>
       <label className='form-label'>Username</label>
       <input title='username' placeholder='Username' type='text' className='form-text-input' />
       <label className='form-label'>Password</label>
       <input title='password' placeholder='Password' type='text' className='form-text-input' />
-      <button>Submit</button>
+      <div className='form-footer'>
+        <button className='form-btn'>Submit</button>
+        <a href='#'>No account yet? Sign up!</a>
+      </div>
     </form>
   );
 };
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -4,13 +4,17 @@ const Signup = () => {
   return (
     <div>
       <form className='form signup'>
+        <h1 className='form-title'>Sign up</h1>
         <label className='form-label'>Username</label>
         <input title='username' placeholder='Username' type='text' className='form-text-input' />
         <label className='form-label'>Email</label>
         <input title='email' placeholder='Email' type='text' className='form-text-input' />
         <label className='form-label'>Password</label>
         <input title='password' placeholder='Password' type='text' className='form-text-input' />
-        <button>Submit</button>
+        <div className='form-footer'>
+          <button className='form-btn'>Submit</button>
+          <a href='#'>Already have an account? Login!</a>
+        </div>
       </form>
     </div>
   );
