3c49167575ab20ce659e744107106c728a178087
Catherine Massarelli <catherine.massarelli@gmail.com>
2022-03-31 12:01:15 +0200

    Remove unnecessary code from routes.

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,17 +1,6 @@
 import cors from 'cors';
 import express, { Application, Router } from 'express';
-import Knex from 'knex';
-import config from '../../knexfile';
-import { Model } from 'objection';
 import { addFavorite, deleteFavorite, getAllFavorites, getAllVideos, login, logout, search, signup } from '../controllers/videos';
-
-
-
-// // Initialize knex
-const knex = Knex(config.development);
-
-// // // Give the knex instance to Objection
-Model.knex(knex);
 
 export const app: Application = express();
 
@@ -32,7 +21,6 @@ const router: Router = express.Router();
 // Add middleware to format data that go to the frontend.
 // Add error handling.
 // Middleware for error handling/ try-catch.
-// Controllers? (like in task manager)
 
 router.route('/videos').get(getAllVideos);
 
